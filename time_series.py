2462cfa546aaf0e7bb667952751cfe09869ca66b
Alexander Fröhling <58947779+netalivo@users.noreply.github.com>
2025-03-02 17:22:44 +0100

    added calculating all arima and naive residuals

--- time_series.py
+++ time_series.py
@@ -5,8 +5,62 @@ import seaborn as sns
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.graphics.tsaplots import plot_acf
 from pandas.plotting import lag_plot as pd_lag_plot
+from model_ARIMA import grid_search_and_build_model
+from model_naiv import build_naive_model
+
+def arima_residuals_for_all_stores(filename):
+
+    df = pd.read_csv(filename, parse_dates=['Date'], dayfirst=True)
+    df.columns = df.columns.str.lower()
+    residuals_dict = {}
+    
+    for store in range(1, 46):
+
+        store_df = df[df['store'] == store].copy()
+        store_df.sort_values('date', inplace=True)
+        store_df.set_index('date', inplace=True)
+        
+        sales = store_df['weekly_sales'].asfreq('W-FRI')
+        
+        try:
+            model_fit = grid_search_and_build_model(sales)
+            fitted_values = model_fit.fittedvalues
+
+            residuals = sales - fitted_values
+            
+            residuals_dict[store] = residuals
+        except Exception as e:
+            print(f"Fehler bei Store {store}: {e}")
+            residuals_dict[store] = None
+            
+    return residuals_dict
 
 
+
+
+def naive_residuals_for_all_stores(filename):
+
+    df = pd.read_csv(filename, parse_dates=['Date'], dayfirst=True)
+    df.columns = df.columns.str.lower()
+    residuals_dict = {}
+    
+    for store in range(1, 46):
+
+        store_df = df[df['store'] == store].copy()
+        store_df.sort_values('date', inplace=True)
+        store_df.set_index('date', inplace=True)
+        
+        sales = store_df['weekly_sales'].asfreq('W-FRI')
+        
+        try:
+            residuals, fitted_values = build_naive_model(sales)
+
+            residuals_dict[store] = residuals
+        except Exception as e:
+            print(f"Fehler bei Store {store}: {e}")
+            residuals_dict[store] = None
+            
+    return residuals_dict
 
 
 def seasonal_plot(sales):
