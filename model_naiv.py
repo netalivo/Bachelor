b472583002ba88a3883fae675582bb3199018df9
Alexander Fröhling <58947779+netalivo@users.noreply.github.com>
2025-05-12 20:18:43 +0200

    changed sarima series length to naiv length to better compare p values, added medians of p values

--- model_naiv.py
+++ model_naiv.py
@@ -6,7 +6,7 @@ import pandas as pd
 def build_naive_model(sales, seasonal_period = 52):
     fitted_values = sales.shift(seasonal_period)
     residuals = sales - fitted_values
-    return residuals, fitted_values
+    return residuals.dropna(), fitted_values
 
 
 def naive_for_all_stores(filename):
